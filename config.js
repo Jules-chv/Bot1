1a621b26c5f94e557edd99dee2a894b5d267b88c
Jules-chv <167612505+Jules-chv@users.noreply.github.com>
2025-01-05 14:05:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ module.exports = {
         buyer: ['1086596778788401182','721039033500827699'], // Buyer
         couleur: '#6495ED', // Couleur HEX
         footer: 'Preppyland', // Dans la commande help ou autre il y'a un texte en bas
-        maxServer: '1', // Le nombre de serveur maximum que le bot peut rejoindre 
+        maxServer: '2', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
 }
