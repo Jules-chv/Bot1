0101806e4ca072ca2319fe98988d5aa299ef101f
Preppy Adam <adammv0294@gmail.com>
2025-01-12 21:42:52 +0100

    Gestion des erreurs ajoutée

--- responses/brat.js
+++ responses/brat.js
@@ -1,13 +1,14 @@
 const Discord = require('discord.js');
-const config = require("../config");
-const db = require('quick.db');
-const cl = new db.table("Color");
 
 module.exports = {
     name: 'br',
     description: `Charli XCX - Brat`,
 
     async execute(client, message, args) {
-        message.channel.send("AT")
+        try {
+            await message.channel.send("AT");
+        } catch (error) {
+            console.error(error);
+        }
     }
 };
